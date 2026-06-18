954d8e35aaaff67ea921a17908e9b6ea069ac441
AtalayKahsay <atalay.kahsay-ug@aau.edu.et>
2026-06-18 01:03:20 -0700

    Fix password reset email configuration

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -128,8 +128,9 @@ exports.forgotPassword = async (req, res) => {
     // Send email
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
+      port: 587,
       secure: false,
+      requireTLS: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
