e389a8734e2f4243408999e25a1523a6e506fd98
AtalayKahsay <atalay.kahsay-ug@aau.edu.et>
2026-06-18 12:04:53 -0700

    Fix Gmail SMTP connection for production

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -127,12 +127,17 @@ exports.forgotPassword = async (req, res) => {
 
     // Send email
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      requireTLS: true,
+      family: 4, // force IPv4
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-      connectionTimeout: 10000, // 10 seconds
+      connectionTimeout: 10000,
+      socketTimeout: 10000,
     });
 
     // Test SMTP connection
